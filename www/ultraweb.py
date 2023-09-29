ac0fc19b4096b10e30e16c00fc9cf0856fc21d0a
Juan M. Casillas <juanm.casillas@gmail.com>
2023-09-29 13:35:42 +0200

    added the basic www template and search

--- www/ultraweb.py
+++ www/ultraweb.py
@@ -1,7 +1,6 @@
-from flask import Flask, render_template, abort, jsonify, send_from_directory
+from flask import Flask, render_template, abort, jsonify, send_from_directory, request
 from flask_bootstrap import  Bootstrap5
-
-
+import base64
 import sys
 sys.path.append('..')
 from ultrastar.songhelper import UltraStarHelper
@@ -21,21 +20,80 @@ def create_app(config_file):
     app.AppEnv = AppEnv
     app.ultrastar_helper =  UltraStarHelper(AppEnv.config())
     app.ultrastar_helper.load_db()
+
+    @app.template_filter()
+    def b64encode(s):
+        return base64.b64encode(s.encode('utf-8'))
+
+    @app.template_filter()
+    def b64decode(s):
+        return base64.b64decode(s).decode()
+
+
     @app.route("/")
     def main():
-        return render_template("artists.html", title="UltraStar Artist List")
+        search = request.args.get('search', default = "", type = str)
+        cursor = app.ultrastar_helper.db.cursor()
+        # using the ID to get the cover is somewhat crap
+        # but works (the cover of any of the song)
+        if search:
+            ## add like string format to ease the search
+            search = "%%%s%%" % search
+            cursor.execute("select artist, id, count(artist) as songs from songs where artist like ? group by(artist) order by artist;",(search,))
+        else:
+            cursor.execute("select artist, id, count(artist) as songs from songs group by(artist) order by artist;")
+        rows = cursor.fetchall()
+        cursor.close()
+        artist_list = list(map(lambda x: dict(x),rows))
+        return render_template("artists.html", 
+                               title="UltraStar Artist List", 
+                               items = artist_list,
+                               search_action = "/")
 
 
-    @app.route("/tdemo")
-    def tdemo():
-        return render_template("tdemo.html", title="UltraStar table demo")
-
-
+    @app.route("/songs")
+    def songs():
+        artist_id = request.args.get('artist', default = "", type = str)
+        search = request.args.get('search', default = "", type = str)
+        title="Ultrastar song list"
+        if artist_id:
+            
+            artist_id = base64.b64decode(artist_id)
+            cursor = app.ultrastar_helper.db.cursor()
+            cursor.execute("select artist from songs where artist=?",(artist_id,))
+            print(artist_id)
+            artist = cursor.fetchone()
+            cursor.close()
+            artist = dict(artist)
+            title = "Ultrastar song list for %s" % artist   
+        
+        
+        return render_template("songs.html", 
+                               title=title, 
+                               artist=artist_id, 
+                               search=search,
+                               search_action = "/songs")
+    
+    
     @app.route("/data")
     def data():
-
+        artist_id = request.args.get('artist', default = "", type = str)
+        search = request.args.get('search', default = "", type = str)
         cursor = app.ultrastar_helper.db.cursor()
-        cursor.execute("select * from songs;")
+       
+        if not search:
+            if artist_id:
+                cursor.execute("select * from songs where artist=? order by title",(artist_id,))
+            else:
+                cursor.execute("select * from songs;")
+        else:
+            ## add like string format to ease the search
+            search = "%%%s%%" % search
+            if artist_id:
+                cursor.execute("select * from songs where artist=? and title like ? order by title",(artist_id,search))
+            else:
+                cursor.execute("select * from songs where title like ?;", (search,))
+        
         rows = cursor.fetchall()
 
         data = []
@@ -45,20 +103,32 @@ def create_app(config_file):
             item['multi'] = "Yes" if item['multi'] else "No"
             data.append(item)
         json = jsonify(data=data)
-        print(json)
         cursor.close()
         return json
+    
+
 
     @app.route('/img/cover/<id>')
-    def download_file(id):
-
+    def serve_img(id):
+        # use the id of the song to get the cover
+        # but use also the cover for the artist
         cursor = app.ultrastar_helper.db.cursor()
-        cursor.execute("select dirname,cover from songs where id=?;",id)
+        cursor.execute("select dirname,cover from songs where id=?;",(id,))
         item = cursor.fetchone()
         if not item:
             abort(404)
-
         return send_from_directory(item["dirname"], item["cover"], as_attachment=False)
+
+    @app.route('/mp3/<id>')
+    def serve_mp3(id):
+        # use the id of the song to get the cover
+        # but use also the cover for the artist
+        cursor = app.ultrastar_helper.db.cursor()
+        cursor.execute("select dirname,mp3 from songs where id=?;",(id,))
+        item = cursor.fetchone()
+        if not item:
+            abort(404)
+        return send_from_directory(item["dirname"], item["mp3"], as_attachment=False)
 
     @app.errorhandler(404)
     def page_not_found(error):
